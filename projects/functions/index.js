88970d293abff90376c2c175b093a6c7a39980b8
Aleksandr Melnick <alexksandr.melnick@gmail.com>
2021-03-02 01:22:23 +0300

    complited 1, 2, 3 tasks

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument(value) {}
+function returnFirstArgument(value) {
+  return value;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,11 @@ function returnFirstArgument(value) {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+
+function sumWithDefaults(a, b) {
+  b = typeof b !== 'undefined' ? b : 100;
+  return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +44,10 @@ function sumWithDefaults(a, b) {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+function returnFnResult(fn) {
+  const a = fn();
+  return a;
+}
 
 /*
  Задание 4:
@@ -47,12 +56,13 @@ function returnFnResult(fn) {}
  При вызове функции F, переданное ранее число должно быть увеличено на единицу и возвращено из F
 
  Пример:
-   var f = returnCounter(10);
-
-   console.log(f()); // выведет 11
+ var f = returnCounter(10);
+ 
+ console.log(f()); // выведет 11
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
+
 function returnCounter(number) {}
 
 /*
