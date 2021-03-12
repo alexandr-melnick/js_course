e07cacc103b06295e9e2314c0f3b981eff3c07e9
Aleksandr Melnick <alexksandr.melnick@gmail.com>
2021-03-12 19:48:48 +0300

    complited homework 3

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -1,17 +1,11 @@
 /* ДЗ 3 - работа с исключениями и отладчиком */
-
-/*
-Задание 1:
-
+/*Задание 1:
 1.1: Функция принимает массив и фильтрующую функцию и должна вернуть true или false
 Функция должна вернуть true только если fn вернула true для всех элементов массива
-
 1.2: Необходимо выбрасывать исключение в случаях:
 - array не массив или пустой массив (с текстом "empty array")
 - fn не является функцией (с текстом "fn is not a function")
-
 Запрещено использовать встроенные методы для работы с массивами
-
 Пример:
 isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
 isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
@@ -112,7 +106,42 @@ function returnBadArguments(fn, ...args) {
 - number не является числом (с текстом "number is not a number")
 - какой-либо из аргументов div является нулем (с текстом "division by 0")
 */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if (typeof number != "number") {
+    throw Error('number is not a number');
+  }
+  let obj = {
+    sum: function () {
+      for (let i = 0; i < arguments.length; i++) {
+        number += arguments[i];
+      }
+      return number;
+    },
+    dif: function () {
+      for (var i = 0; i < arguments.length; i++) {
+        number -= arguments[i];
+      }
+      return number;
+    },
+    div: function () {
+      for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] === 0) {
+          throw Error('division by 0');
+        }
+        number /= arguments[i];
+      }
+      return number;
+    },
+    mul: function () {
+      for (let i = 0; i < arguments.length; i++) {
+        number *= arguments[i];
+      }
+      return number;
+    }
+  }
+  return obj;
+}
+
 
 /* При решении задач, постарайтесь использовать отладчик */
 
