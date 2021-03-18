24b1681d878dcaaadeba53ada5dc9b1b2aadacf1
Aleksandr Melnick <alexksandr.melnick@gmail.com>
2021-03-18 23:15:11 +0300

    comlited homework 5

--- projects/async/index.js
+++ projects/async/index.js
@@ -1,58 +1,63 @@
 /*
- Страница должна предварительно загрузить список городов из
- https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- и отсортировать в алфавитном порядке.
-
- При вводе в текстовое поле, под ним должен появляться список тех городов,
- в названии которых, хотя бы частично, есть введенное значение.
- Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
-
- Во время загрузки городов, на странице должна быть надпись "Загрузка..."
- После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
-
- Разметку смотрите в файле towns.html
-
- Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
-
- *** Часть со звездочкой ***
- Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
- то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
- При клике на кнопку, процесс загрузки повторяется заново
- */
+Страница должна предварительно загрузить список городов из
+https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+и отсортировать в алфавитном порядке.
+При вводе в текстовое поле, под ним должен появляться список тех городов,
+в названии которых, хотя бы частично, есть введенное значение.
+Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+Разметку смотрите в файле towns.html
+Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+*** Часть со звездочкой ***
+Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+При клике на кнопку, процесс загрузки повторяется заново
+*/
 
 /*
- homeworkContainer - это контейнер для всех ваших домашних заданий
- Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
-
- Пример:
-   const newDiv = document.createElement('div');
-   homeworkContainer.appendChild(newDiv);
- */
+homeworkContainer - это контейнер для всех ваших домашних заданий
+Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+Пример:
+const newDiv = document.createElement('div');
+homeworkContainer.appendChild(newDiv);
+*/
 
 import './towns.html';
 
 const homeworkContainer = document.querySelector('#app');
 
 /*
- Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
-
- Массив городов пожно получить отправив асинхронный запрос по адресу
- https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- */
-function loadTowns() {}
+Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+Массив городов можно получить отправив асинхронный запрос по адресу
+https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+*/
+function loadTowns() {
+  return new Promise((resolve) => {
+    fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+      .then((response) => response.json())
+      .then((towns) => {
+        const sortTowns = towns.sort((a, b) => {
+          return a.name < b.name ? -1 : 1;
+        });
+        resolve(sortTowns);
+      });
+  });
+}
 
 /*
- Функция должна проверять встречается ли подстрока chunk в строке full
- Проверка должна происходить без учета регистра символов
-
- Пример:
-   isMatching('Moscow', 'moscow') // true
-   isMatching('Moscow', 'mosc') // true
-   isMatching('Moscow', 'cow') // true
-   isMatching('Moscow', 'SCO') // true
-   isMatching('Moscow', 'Moscov') // false
- */
-function isMatching(full, chunk) {}
+Функция должна проверять встречается ли подстрока chunk в строке full
+Проверка должна происходить без учета регистра символов
+Пример:
+ isMatching('Moscow', 'moscow') // true
+ isMatching('Moscow', 'mosc') // true
+ isMatching('Moscow', 'cow') // true
+ isMatching('Moscow', 'SCO') // true
+ isMatching('Moscow', 'Moscov') // false
+*/
+function isMatching(full, chunk) {
+  return full.toUpperCase().includes(chunk.toUpperCase());
+}
 
 /* Блок с надписью "Загрузка" */
 const loadingBlock = homeworkContainer.querySelector('#loading-block');
@@ -66,9 +71,28 @@ const filterBlock = homeworkContainer.querySelector('#filter-block');
 const filterInput = homeworkContainer.querySelector('#filter-input');
 /* Блок с результатами поиска */
 const filterResult = homeworkContainer.querySelector('#filter-result');
-
+const createUl = document.createElement('ul');
+filterBlock.appendChild(createUl);
+loadTowns().then((sortTowns) => {
+  filterBlock.style.display = 'block';
+  loadingBlock.style.display = 'none';
+  loadingFailedBlock.firstElementChild.style.display = 'none';
+  filterInput.addEventListener('input', function (e) {
+    const chunk = e.target.value;
+    filterResult.textContent = '';
+    for (let i = 0; i < sortTowns.length; i++) {
+      const item = sortTowns[i].name;
+      if (item.toLowerCase().match(chunk.toLowerCase())) {
+        const createDiv = document.createElement('div');
+        createDiv.textContent = item;
+        filterResult.appendChild(createDiv);
+      }
+      if (!chunk) {
+        filterResult.textContent = '';
+      }
+    }
+  });
+});
 retryButton.addEventListener('click', () => {});
-
-filterInput.addEventListener('input', function () {});
 
 export { loadTowns, isMatching };
